b74dbd062596fd231fb4667f519014a757bd7e7a
Christopher Degawa <christopher.degawa@intel.com>
2020-08-13 14:33:16 +0000

    CMake: Don't exclude msvc and xcode from installing
    BUG=aomedia:2778
    
    Change-Id: Ic2c8be1214d2cbf6d23dcf0a269d580b35e1bf02

--- build/cmake/aom_install.cmake
+++ build/cmake/aom_install.cmake
@@ -27,70 +27,68 @@ endif()
 # Note: aom.pc generation uses GNUInstallDirs:
 # https://cmake.org/cmake/help/latest/module/GNUInstallDirs.html
 macro(setup_aom_install_targets)
-  if(NOT (MSVC OR XCODE))
-    include("GNUInstallDirs")
-    set(AOM_PKG_CONFIG_FILE "${AOM_CONFIG_DIR}/aom.pc")
+  include("GNUInstallDirs")
+  set(AOM_PKG_CONFIG_FILE "${AOM_CONFIG_DIR}/aom.pc")
 
-    # Create a dummy library target for creating aom.pc.
-    create_dummy_source_file(aom_pc c AOM_PKG_CONFIG_SOURCES)
-    add_library(aom_pc ${AOM_PKG_CONFIG_SOURCES})
+  # Create a dummy library target for creating aom.pc.
+  create_dummy_source_file(aom_pc c AOM_PKG_CONFIG_SOURCES)
+  add_library(aom_pc ${AOM_PKG_CONFIG_SOURCES})
 
-    # Setup a rule to generate aom.pc.
-    add_custom_command(
-      OUTPUT "${AOM_PKG_CONFIG_FILE}"
-      COMMAND ${CMAKE_COMMAND} ARGS
-              -DAOM_CONFIG_DIR=${AOM_CONFIG_DIR} -DAOM_ROOT=${AOM_ROOT}
-              -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-              -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
-              -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
-              -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
-              -DCMAKE_PROJECT_NAME=${CMAKE_PROJECT_NAME}
-              -DCONFIG_MULTITHREAD=${CONFIG_MULTITHREAD}
-              -DHAVE_PTHREAD_H=${HAVE_PTHREAD_H} -P
-              "${AOM_ROOT}/build/cmake/pkg_config.cmake"
-      COMMENT "Writing aom.pc"
-      VERBATIM)
+  # Setup a rule to generate aom.pc.
+  add_custom_command(
+    OUTPUT "${AOM_PKG_CONFIG_FILE}"
+    COMMAND ${CMAKE_COMMAND} ARGS
+            -DAOM_CONFIG_DIR=${AOM_CONFIG_DIR} -DAOM_ROOT=${AOM_ROOT}
+            -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+            -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
+            -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
+            -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
+            -DCMAKE_PROJECT_NAME=${CMAKE_PROJECT_NAME}
+            -DCONFIG_MULTITHREAD=${CONFIG_MULTITHREAD}
+            -DHAVE_PTHREAD_H=${HAVE_PTHREAD_H} -P
+            "${AOM_ROOT}/build/cmake/pkg_config.cmake"
+    COMMENT "Writing aom.pc"
+    VERBATIM)
 
-    # Explicitly add a dependency on the pkg-config file to ensure it's built.
-    get_property(aom_pc_sources TARGET aom_pc PROPERTY SOURCES)
-    set_source_files_properties(${aom_pc_sources} OBJECT_DEPENDS
-                                "${AOM_PKG_CONFIG_FILE}")
+  # Explicitly add a dependency on the pkg-config file to ensure it's built.
+  get_property(aom_pc_sources TARGET aom_pc PROPERTY SOURCES)
+  set_source_files_properties(${aom_pc_sources} OBJECT_DEPENDS
+                              "${AOM_PKG_CONFIG_FILE}")
 
-    # Our pkg-config file carries version information: add a dependency on the
-    # version rule.
-    add_dependencies(aom_pc aom_version)
+  # Our pkg-config file carries version information: add a dependency on the
+  # version rule.
+  add_dependencies(aom_pc aom_version)
 
-    if(CONFIG_AV1_DECODER)
-      if(ENABLE_EXAMPLES)
-        list(APPEND AOM_INSTALL_BINS aomdec)
-      endif()
-    endif()
-
-    if(CONFIG_AV1_ENCODER)
-      if(ENABLE_EXAMPLES)
-        list(APPEND AOM_INSTALL_BINS aomenc)
-      endif()
-    endif()
-
-    if(BUILD_SHARED_LIBS)
-      set(AOM_INSTALL_LIBS aom aom_static)
-    else()
-      set(AOM_INSTALL_LIBS aom)
-    endif()
-
-    # Setup the install rules.
-    install(
-      FILES ${AOM_INSTALL_INCS}
-      DESTINATION "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}/aom")
-    install(
-      FILES "${AOM_PKG_CONFIG_FILE}"
-      DESTINATION "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/pkgconfig")
-    install(TARGETS ${AOM_INSTALL_LIBS} DESTINATION
-                    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
-
+  if(CONFIG_AV1_DECODER)
     if(ENABLE_EXAMPLES)
-      install(TARGETS ${AOM_INSTALL_BINS} DESTINATION
-                      "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}")
+      list(APPEND AOM_INSTALL_BINS aomdec)
     endif()
+  endif()
+
+  if(CONFIG_AV1_ENCODER)
+    if(ENABLE_EXAMPLES)
+      list(APPEND AOM_INSTALL_BINS aomenc)
+    endif()
+  endif()
+
+  if(BUILD_SHARED_LIBS)
+    set(AOM_INSTALL_LIBS aom aom_static)
+  else()
+    set(AOM_INSTALL_LIBS aom)
+  endif()
+
+  # Setup the install rules.
+  install(
+    FILES ${AOM_INSTALL_INCS}
+    DESTINATION "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}/aom")
+  install(
+    FILES "${AOM_PKG_CONFIG_FILE}"
+    DESTINATION "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/pkgconfig")
+  install(TARGETS ${AOM_INSTALL_LIBS} DESTINATION
+                  "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+
+  if(ENABLE_EXAMPLES)
+    install(TARGETS ${AOM_INSTALL_BINS} DESTINATION
+                    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}")
   endif()
 endmacro()
